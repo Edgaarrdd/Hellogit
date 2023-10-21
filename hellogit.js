eef81817551108c679dd0fe7f8485ef6d752d07f
Edgard <edgardmachucacode@gmail.com>
2023-10-20 22:34:47 -0300

    arreglado hellogit

--- hellogit.js
+++ hellogit.js
@@ -1,11 +1,5 @@
-console.log("Hello Git")
-<<<<<<< HEAD
-const producto {
-    casa : "la florida",
-    pasaje: "Cerro blanco"
-}
-=======
+
 const producto = {
     televisores : "camino"
-};
->>>>>>> main
+    
+}
